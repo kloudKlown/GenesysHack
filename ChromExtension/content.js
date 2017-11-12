3523b46a23e0a0af92b5f5b2ee920ac5f33e1402
unknown <suhas.servesh@gmail.com>
2017-11-12 00:30:05 -0800

    12: cleaning Content.js

--- ChromExtension/content.js
+++ ChromExtension/content.js
@@ -26,45 +26,11 @@ var r = setInterval(function(){
         but.addEventListener('click',function(){
         	window.parent.postMessage({ startRecording: true }, '*');
         });
-        clearInterval(r);
-        
+        clearInterval(r);       
     });
-
-
 }, 1000);
 
 
-var message = "";
-
-
-// var rr = setInterval(function(){
-//      var phrase = j.each(j("#phraseDiv"), function( index, value ) {
-
-//     	// console.log(POC.$(this))
-// 		var div = j(this);
-// 		console.log(div);
-// 		return div
-// 		// console.log( POC.$(this).find("#phraseDiv").val().length);
-
-//         // clearInterval(rr);	
-// 		// div.addEventListener('change',function(){
-// 		// 		console.log('inside change event');
-// 		// 		// console.log(POC.$(this).val());
-// 		// 		message = j(this).val();
-// 		// 	})
-
-
-//         // clearInterval(rr);
-        
-//     });
-//     if( phrase.val() != undefined ){
-// 	message = phrase.val();
-// 	console.log(phrase.val());
-
-//     }
-//     // clearInterval(rr);
-
-// }, 10000);
 
 	 window.addEventListener("message",function(event){
 	 	if (event.data.phrase) {
@@ -77,36 +43,3 @@ var message = "";
     });
 
 
-console.log(message);
-
-// var r2 = setInterval(function(){
-// 	var input = j.each(j('#wc0-w2-input'), function( index, value ) {
-//         console.log("HACKATHON - FOUND CONVERSATION");
-
-//         return j(this);
-//         // console.log($('#phraseDiv', window.parent));
-//         // // window.parent
-//         // console.log(message);
-//         // console.log( j(this) );
-//         // // console.log(leng);
-//         // j(this).val(message);
-//         // console.log(j(this));        
-//         // clearInterval(r2);
-//         // return j(this).val(message).length;
-
-//     });
-//     console.log(message, message.length);
-//     // input[0].val(message)
-
-//     // console.log(input[0]);
-//     // if( message.length > 0 ){
-// 	// message = input.val();
-// 	input.val(message);
-
-//     }
-//  	// console.log(le);
-
-// }, 1000);
-
-
-// console.log(POC.$('#wc0-w2-input'))
